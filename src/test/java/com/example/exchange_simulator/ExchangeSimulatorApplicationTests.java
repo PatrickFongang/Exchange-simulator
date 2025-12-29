87789fc4aedc0ab81578a066634ec0e83f8db023
LegalnyPoeta <pfongang2005@gmail.com>
2025-12-29 11:11:19 +0100

    first commit

---
+++ src/test/java/com/example/exchange_simulator/ExchangeSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exchange_simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
