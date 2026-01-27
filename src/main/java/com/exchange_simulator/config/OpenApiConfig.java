e804ff52efa9a9d22c231448cce7696efd2a3095
JohnThePenguin <jas.ancuta@gmail.com>
2026-01-27 23:51:50 +0100

    Swagger and OpenApi automagically generated

---
+++ src/main/java/com/exchange_simulator/config/OpenApiConfig.java
@@ -0,0 +1,18 @@
+package com.exchange_simulator.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI apiInfo() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Exchange Simulator Docs")
+                        .version("1.0")
+                        .description("API Docs"));
+    }
+}
