2baa14cad981b1b8978d42c496905c4885b761cf
JohnThePenguin <jas.ancuta@gmail.com>
2026-01-28 12:13:37 +0100

    Cors and users/me endpoint

--- src/main/java/com/exchange_simulator/config/SecurityConfig.java
+++ src/main/java/com/exchange_simulator/config/SecurityConfig.java
@@ -10,6 +10,12 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableMethodSecurity
@@ -19,7 +25,7 @@ public class SecurityConfig {
 
         http
                 .csrf(AbstractHttpConfigurer::disable)
-
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(request -> request
                         .requestMatchers("/", "/api/auth/login", "/api/auth/registration", "/error", "/css/**",
                         "/swagger-ui/**", "/v3/**").permitAll()
@@ -33,5 +39,20 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+        configuration.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
--- src/main/java/com/exchange_simulator/controller/UserController.java
+++ src/main/java/com/exchange_simulator/controller/UserController.java
@@ -2,10 +2,12 @@ package com.exchange_simulator.controller;
 
 import com.exchange_simulator.dto.user.UserResponseDto;
 import com.exchange_simulator.exceptionHandler.exceptions.database.UserNotFoundException;
+import com.exchange_simulator.security.CustomUserDetails;
 import com.exchange_simulator.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,6 +26,15 @@ public class UserController {
                 .map(UserService::getDto)
                 .toList());
     }
+
+    @GetMapping("/me")
+    public ResponseEntity<UserResponseDto> getMe(@AuthenticationPrincipal CustomUserDetails user){
+        var found = userService.getUserById(user.getId());
+        return found
+                .map(value -> ResponseEntity.ok().body(UserService.getDto(value)))
+                .orElseThrow(() -> new UserNotFoundException(user.getId()));
+    }
+
     @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/{id}")
     public ResponseEntity<UserResponseDto> getUser(@PathVariable("id") Long id){
